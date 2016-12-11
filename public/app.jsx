05fb3756ff3cfa0e7986f8b5f591a3eef1c92ab4
kartik <khandelwal.kartik4@gmail.com>
2016-12-11 23:40:13 +0530

    webpack included !
    Signed-off-by: kartik <khandelwal.kartik4@gmail.com>

--- public/app.jsx
+++ public/app.jsx
@@ -1,76 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('Greeter');
 
-var GreetingMessage = React.createClass({
-    render : function () {
-        var name = this.props.name;
-        var message = this.props.message;
-        return (
-          <div>
-              <h1> {name}</h1>
-              <p> {message}</p>
-          </div>
-        );
-    }
-});
-
-var GreeterForm = React.createClass({
-
-    onFormSubmit:function (e) {
-        var update = {};
-        e.preventDefault();
-        var name = this.refs.name.value;
-        var message = this.refs.message.value;
-        if(name.length>0){
-            this.refs.name.value='';
-            update.name = name;
-        }
-
-        if(message.length>0){
-            this.refs.message.value = '';
-            update.message = message;
-        }
-        this.props.onNewName(update);
-    },
-    render : function () {
-        return(
-            <form onSubmit={this.onFormSubmit}>
-                <input type="name" placeholder="Enter name" ref="name"/>
-                <textarea type="message" placeholder="Enter Message" ref="message"/>
-                <button> Set name </button>
-
-            </form>
-        );
-    }
-
-});
-var Greeting = React.createClass({
-    getDefaultProps: function () {
-      return {
-             name : 'React',
-             Message : "This is Default Messages"
-      }
-    },
-    getInitialState : function () {
-      return {
-          name:this.props.name,
-          message:this.props.message
-      };
-    },
-    handelNewName : function (update) {
-       this.setState(update)
-    },
-    render: function () {
-        var name = this.state.name;
-        var message = this.state.message;
-        return(
-            <div>
-                <GreetingMessage name={name} message ={message}/>
-               <GreeterForm onNewName={this.handelNewName}/>
-            </div>
-        )
-    }
-});
+var firstName = 'Kartik ';
 
 ReactDOM.render(
-    <Greeting name="kartik" message="Welcome"/>,
-    document.getElementById("app")
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
 );
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
+
+// Create our app
 var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function(err){
-    if (err) throw err;
-    console.log("Server is Running on port " + 3000);
+app.listen(3000, function () {
+  console.log('Express server is up on port 3000');
 });
-
